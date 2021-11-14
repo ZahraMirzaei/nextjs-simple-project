fa5450188fed4e18aa9ba106f9ebf9b8c7745bfc
ZahraMirzaei.JS <z.mirzaei2019@gmail.com>
2021-11-14 09:23:41 +0330

    nextjs project

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,51 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails({ meetupData }) {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="some street 5, some city"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch from server
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "First Meetup",
+        address: "some street 5, some city",
+        description: "This is a first meetup",
+      },
+    },
+    // revalidate: 10,
+  };
+}
+
+export default MeetupDetails;
